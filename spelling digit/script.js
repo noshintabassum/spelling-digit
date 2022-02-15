e7a3acdee9d31a46d1ff1cbae5a14a444fb11583
Noshin Tabassum <81520766+noshintabassum@users.noreply.github.com>
2022-02-15 21:26:46 +0600

    Add files via upload

---
+++ spelling digit/script.js
@@ -0,0 +1,39 @@
+var digit = prompt("enter your digit : ");
+
+switch (digit) {
+
+    case "0":
+        document.write("spelling of " + digit + " : zero");
+        break;
+    case "1":
+        document.write("spelling of " + digit + " : one");
+        break;
+    case "2":
+        document.write("spelling of " + digit + " : two");
+        break;
+    case "3":
+        document.write("spelling of " + digit + " : three");
+        break;
+    case "4":
+        document.write("spelling of " + digit + " : four");
+        break;
+    case "5":
+        document.write("spelling of " + digit + " : five");
+        break;
+    case "6":
+        document.write("spelling of " + digit + " : six");
+        break;
+    case "7":
+        document.write("spelling of " + digit + " : seven");
+        break;
+    case "8":
+        document.write("spelling of " + digit + " : eight");
+        break;
+    case "9":
+        document.write("spelling of " + digit + " : nine");
+        break;
+    default:
+        document.write(digit + " is not a digit..........");
+
+
+}
